c4a04b302b769aeb9768cdf14b366d80505b2926
garry911128 <garry80633@gmail.com>
2024-02-11 23:23:16 +0800

    temp

---
+++ Assets/Scripts/BattleScenes/Player/Card/Card.cs
@@ -0,0 +1,56 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public int No;// 卡牌編號
+    public string cardName;// 卡牌名称
+    public int damage;
+    public int cost;
+    public Image CardImage;
+    public TextMeshProUGUI cardText;
+    public Text cardCost;
+    public Enemy enemy;
+    private Vector3 enlargedScale = new Vector3(2f, 2f, 2f);
+    private Vector3 hoverOffset = new Vector3(0f, 40f, 0f); // 向上移动的偏移量
+
+    private void Update()
+    {
+        if (No == 1)
+        {
+            
+            cardText.text = "對敵人造成 <color=yellow>" + damage.ToString() + "</color> 點傷害";
+            cardCost.text = cost.ToString();
+        }
+        if (No == 2)
+        {
+
+            cardText.text = "增加玩家 <color=yellow>5</color> 點護盾值";
+            cardCost.text = cost.ToString();
+        }
+        if (No == 3)
+        {
+
+            cardText.text = "回復 <color=yellow>3</color> 點能量";
+            cardCost.text = cost.ToString();
+        }
+    }
+
+    public void TriggerEffect(int cardNo)
+    {
+        if (cardNo == 1)
+        {
+            enemy.TakeDamage(10);
+        }
+        else if (cardNo == 2)
+        {
+            enemy.TakeDamage(10);
+        }
+        
+    }
+
+    
+
+}
+
---
+++ Assets/Scripts/BattleScenes/Player/PlayerConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.BattleScenes.Player
+{
+    public class PlayerConstants
+    {
+        public const int MAX_PLAYER_HP = 100;
+    }
+}
